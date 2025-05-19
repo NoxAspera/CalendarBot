52e74f175bca564abcfb50e981d181ed559c963b
NoxAspera <noxaspera@NoxAsperas-MacBook-Pro.local>
2025-05-19 15:31:34 -0600

    trying out sync

--- src/server.js
+++ src/server.js
@@ -110,6 +110,7 @@ router.post('/', async (request, env) => {
       }
       case SYNC.name.toLowerCase():
         {
+          console.log(google.calendar(calendarOptions).calendarList)
           return new JsonResponse({
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             data: {
