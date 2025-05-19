80beb974d0d2b8e5b6a19c936cc9c24661f93e20
NoxAspera <noxaspera@NoxAsperas-MacBook-Pro.local>
2025-05-19 14:01:32 -0600

    testing

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ router.get('/', (request, env) => {
 });
 
 router.get('/oauth2flow', async (request, env) => {
-  return new Response(await request.params)
+  return new Response(await request.params.code)
 })
 
 /**
